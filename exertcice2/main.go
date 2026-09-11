8cc42b976b9c7b5df6ddecf1c56194231cafb24e
hmohamed <mohamedamine.habily@ynov.com>
2026-09-11 16:16:44 +0200

    Ajoute la saisie des notes

--- exertcice2/main.go
+++ exertcice2/main.go
@@ -8,4 +8,10 @@ func main() {
 	var nombreNotes int
 	fmt.Print("Combien de notes voulez-vous saisir ? ")
 	fmt.Scan(&nombreNotes)
+
+	notes := make([]int, nombreNotes)
+	for i := range notes {
+		fmt.Printf("Note %d : ", i+1)
+		fmt.Scan(&notes[i])
+	}
 }
